a2dbb4238679e567781c735382dec26039012564
Herianto Tanoyo <heri2000@live.com>
2022-07-18 21:27:41 +0700

    Display file original name, file mimetype and file size

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ require('dotenv').config()
 
 var app = express();
 
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `upfile` file
+  // req.body will hold the text fields, if there were any
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 
